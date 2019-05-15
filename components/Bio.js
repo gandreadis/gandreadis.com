d1ae72844d23983471f34951401ea4d96bab0121
Georgios Andreadis <G.Andreadis@student.tudelft.nl>
2019-05-15 19:32:38 +0200

    Update Bio.js

--- components/Bio.js
+++ components/Bio.js
@@ -46,7 +46,7 @@ export default () => (
             </BioItem>
             <BioItem
                 emoji="🎵"
-                title="Principal Viola"
+                title="Viola Player"
                 subtitle="Ricciotti & Krashna"
             >
                 I am currently principal violist in the <a href="https://ricciotti.nl" className="has-text-weight-bold">Ricciotti Ensemble</a> and the Delft student symphony orchestra <a href="https://krashna.nl" className="has-text-weight-bold">Krashna Musika</a>.
