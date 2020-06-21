c0a3e53a055ca5b811a901fbbaeddd3a56ba8602
Georgios Andreadis <info@gandreadis.com>
2020-06-21 11:52:11 +0200

    Remove sharps

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-remove-trailing-slashes`,
     `gatsby-plugin-sitemap`,
